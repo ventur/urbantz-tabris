73051dcc7c1479737bd76365d3ab5757fedb21f3
Daniel <dventuramorales@adneombenelux.com>
2016-07-06 18:18:57 +0200

    Added map and markers

--- pages/map.js
+++ pages/map.js
@@ -1,7 +1,7 @@
 var Utils = require('../services/utils')
 var moment = require('momentjs')
 
-var user, page, ready = false;
+var round, page, ready = false;
 
 function init(_data) {
     round = _data.round
@@ -14,9 +14,22 @@ function init(_data) {
 
 function open() {
     var map = tabris.create("ESMap", {
-        layoutData: {left: 0, right: 0, top: 0, height: 200}
+        layoutData: {left: 0, right: 0, top: 0, bottom: 0}
     }).on("ready", function() {
-        this.set("center", [-33.867, 151.206]);
+        var m = this;
+        round.stops.forEach(function(stop) {
+            if (stop.stopType === 0) {
+                m.createMarker({
+                    id: stop.parcel,
+                    latLng: [stop.coordinates[1], stop.coordinates[0]],
+                    title: stop.parcel,
+                    color:"blue"
+                }).on("tap", function() {
+                    console.log(this.id);
+                });
+            }
+        })
+        this.set("center", [round.stops[1].coordinates[1], round.stops[1].coordinates[0]]);
         this.set("zoom", 13);
     }).appendTo(page);
 
