06d13e53032aaf5e14a77fd4edfbb71ccee1c582
Daxtek <dieudonne.loic@gmail.com>
2016-07-06 16:31:20 +0200

    login page layout fix

--- pages/login.js
+++ pages/login.js
@@ -16,13 +16,15 @@ function open() {
     var pass = 'Abcd1234'
 
     var textViewInfo = new tabris.TextView({
-        layoutData: {centerX: 0 , centerY: -40},
+        layoutData: {centerX: 0, top: 15},
+        font: '24px',
+        percentage: '60%',
         text: 'Info'
     }).appendTo(page);
 
 
-    new tabris.TextInput({
-        layoutData: {centerX: 0 , centerY: 0 , width: 200},
+    var emailInput = new tabris.TextInput({
+        layoutData: {centerX: 0, top: [textViewInfo, 20], width: 200},
         message: "Email",
         text: email
     }).on("accept", function(widget, text) { // Fired when a text input has been finished by pressing the keyboard's Enter key.
@@ -30,8 +32,8 @@ function open() {
         }).appendTo(page);
 
 
-    new tabris.TextInput({
-        layoutData: {centerX: 0 , centerY: 40 , width: 200},
+    var passwordInput = new tabris.TextInput({
+        layoutData: {centerX: 0, top: [emailInput, 20] , width: 200},
         message: "Enter password",
         type: 'password',
         text: pass
@@ -40,7 +42,7 @@ function open() {
         }).appendTo(page);
 
     new tabris.Button({
-        layoutData: {centerX: 0 , centerY: 80 , width: 200},
+        layoutData: {centerX: 0, top: [passwordInput, 20] , width: 200},
         text: "Button"
     }).on("select", function() {
             Promise.resolve(true).then(function(res){
